b777a01393d62790f1022ae925b0c3b9cb6240a1
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-06-29 11:50:07 -0500

    added tab directive

---
+++ app.js
@@ -0,0 +1,17 @@
+;(function(window) {
+
+angular.module('app', [])
+
+.directive('tab', function() {
+	return {  // This is a DDO, directive definition object
+		restrict: 'E',
+		transclude: true,
+		template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+		scope: {}, // isolated scope
+		link: function(scope, elem, attr) {
+			
+		}
+	}
+})
+
+})(window);
