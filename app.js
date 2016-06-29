80fbee4ff250d28f2927b990e0f70c9288d848ac
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-06-29 12:12:12 -0500

    added addTab fxn

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ angular.module('app', [])
 		require: '^tabset', // instructs directive to move up the scope hierarchy one level and look for controller
 		scope: {}, // isolated scope
 		link: function(scope, elem, attr, tabsetCtrl) {
-
+			tabsetCtrl.addTab(scope);
 		}
 	}
 })
@@ -27,6 +27,10 @@ angular.module('app', [])
 			// using a controller allows us to inject the tabset controller instance into each of the tab link functions
 			var self = this; // ES6???
 			self.tabs = []
+
+			self.addTab = function addTab(tab) {
+				self.tabs.push(tab);
+			}
 		}
 	}
 })
