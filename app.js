7b22762ab48034792387289b9c277f0b889b68cb
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-06-29 12:07:56 -0500

    injected tabset into tab

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ angular.module('app', [])
 		restrict: 'E',
 		transclude: true,
 		template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+		require: '^tabset', // instructs directive to move up the scope hierarchy one level and look for controller
 		scope: {}, // isolated scope
-		link: function(scope, elem, attr) {
+		link: function(scope, elem, attr, tabsetCtrl) {
 
 		}
 	}
@@ -23,6 +24,7 @@ angular.module('app', [])
 		bindToController: 'true',
 		controllerAs: 'tabset', // allows us to bind properties directly to controller. Removes dependency on $scope
 		controller: function() {
+			// using a controller allows us to inject the tabset controller instance into each of the tab link functions
 			var self = this; // ES6???
 			self.tabs = []
 		}
