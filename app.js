be9f4fe6f74575f784defab8c57edbcb4daac8a3
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-06-29 11:57:52 -0500

    add tabset directive

--- app.js
+++ app.js
@@ -9,7 +9,22 @@ angular.module('app', [])
 		template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
 		scope: {}, // isolated scope
 		link: function(scope, elem, attr) {
-			
+
+		}
+	}
+})
+
+.directive('tabset', function() {
+	return {
+		restrict: 'E',
+		transclude: 'true',
+		scope: {},
+		templateUrl: 'tabset.html',
+		bindToController: 'true',
+		controllerAs: 'tabset', // allows us to bind properties directly to controller. Removes dependency on $scope
+		controller: function() {
+			var self = this; // ES6???
+			self.tabs = []
 		}
 	}
 })
